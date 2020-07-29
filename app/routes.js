1e412bd5ae8797647e3be252ae5dceb1713bcde4
Robbie Paul <robbie.paul@dxw.com>
2020-07-29 16:53:35 +0100

    Add basic "Find someone" search page

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.use(function (req, res, next) {
+    res.locals.query = req.query
+    next()
+})
+
 
 module.exports = router
