b8243c2bd94afa393744bad01b9b848cb0f87a3a
Robbie Paul <robbie.paul@dxw.com>
2020-09-28 11:54:38 +0100

    Update list of prototypes to include everything

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'PPUD Alpha prototypes',
+  serviceName: 'PPUD Alpha',
 
   // Default port that prototype runs on
   port: '3000',
